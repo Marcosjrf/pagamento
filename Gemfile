24d638bd9224d204ad6b0021e274c25b799a355f
marcos <marcos@mymail>
2011-10-07 10:41:38 -0300

    inicio

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+
+gem 'sqlite3', group: :development
+gem 'heroku', group: :development
+gem 'pg', group: :production
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+gem 'therubyracer'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pagamento::Application.config.secret_token = '68cfa46f5f0facdb394c906165746b2b25dbf96f0dfa15731be0576743e05229ea388870390b2e11ceba3a942285a9415113266786d633b5ebe19c0985418262'
