601ad911a968eef6328880000e61783404443dc4
marcos <marcos@mymail>
2011-10-17 09:12:45 -0300

    separei cnpj e cpf tabela fornecedores

---
+++ db/migrate/20111017115636_separar_cpf_e_cnpj.rb
@@ -0,0 +1,8 @@
+class SepararCpfECnpj < ActiveRecord::Migration
+  def change
+  	rename_column :fornecedores, :cnpj_cpf, :cnpj
+  	add_column :fornecedores, :cpf, :string
+  	add_column :fornecedores, :pj, :boolean  
+  end
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111010150001) do
+ActiveRecord::Schema.define(:version => 20111017115636) do
 
   create_table "fornecedores", :force => true do |t|
-    t.string   "cnpj_cpf"
+    t.string   "cnpj"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "nome"
+    t.string   "cpf"
+    t.boolean  "pj"
   end
 
   create_table "usuarios", :force => true do |t|
